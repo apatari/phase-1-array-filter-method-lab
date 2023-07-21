67dcea123c9ff3920c5e8df5136fe88ae0b78ca6
Andy Patari <andy.patari@gmail.com>
2023-07-21 10:41:09 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(arr, str) {
+    return arr.filter(s => s.toUpperCase() === str.toUpperCase())
+}
+
+
+function fuzzyMatch(arr, str) {
+    return arr.filter(s => s.startsWith(str))
+}
+
+function matchName(arr, str) {
+    return arr.filter(item => item.name === str)
+}
